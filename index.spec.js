8a91926e71450387f14c48b6a47c331dbb20b8a4
Nilesh Patel <nilesh.nvs@hotmail.com>
2020-12-04 09:59:33 +0530

    feat: new function add (#1)

--- index.spec.js
+++ index.spec.js
@@ -1,7 +1,11 @@
-const { author } = require('./index.js')
+const { author, add } = require('./index.js')
 
 describe('Test cases', () => {
     test('Author name should be correct', () => {
         expect(author).toEqual('Nilesh Patel')
+    })
+
+    test('add', ()=>{
+        expect(add(1,2)).toEqual(3)
     })
 })
