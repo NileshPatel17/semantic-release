6d7eb85c40df04065b69ec1f6b2565fdc712f989
Nilesh Patel <nilesh.nvs@hotmail.com>
2020-12-04 09:58:01 +0530

    feat: new function add

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const { author, name, version } = require('./package.json')
 
+function add(a,b){
+    return a+b;
+}
+
 module.exports = {
     author,
     name,
-    version
+    version,
+    add
 }
