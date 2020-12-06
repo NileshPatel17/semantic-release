1e2b0cdcc0427102edc95ab8708fcd6f57ae75b7
Nilesh Patel <nilesh.nvs@hotmail.com>
2020-12-06 11:35:12 +0530

    feat: new function TitleCase
    closes #6
    
    #6

--- index.ts
+++ index.ts
@@ -15,8 +15,15 @@ export function beta(): string {
     return `version: ${version}`
 }
 
+export function TitleCase(str: String): String{
+    if(!str){
+        throw new Error('invalid input')
+    }
+    return str.split(' ').map(item=> item.slice(0,1).toUpperCase()+item.slice(1)).join(' ')
+}
+
 export {
     author,
     packageName,
-    version,
+    version
 }
